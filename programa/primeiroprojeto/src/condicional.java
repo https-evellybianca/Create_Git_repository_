5ac4c5dd5896cda9773c7228b1fcf621c9c1a289
https-evellybianca <127303756+https-evellybianca@users.noreply.github.com>
2024-09-12 17:40:36 -0300

    Add files via upload

---
+++ programa/primeiroprojeto/src/condicional.java
@@ -0,0 +1,12 @@
+public class condicional {
+    public static void main(String[] args) {
+        int anoDeLancamento = 2022;
+        boolean incluidoNoPlano = true;
+        double notaDoFilme = 9.5;
+
+        if (anoDeLancamento > 2022){
+            System.out.println("Lançamento que os clientes estão curtindo!");
+        }
+
+    }
+}
